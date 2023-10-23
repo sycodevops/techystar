168c07c6b912b708d18698c94186a11745e277c8
Sachin <110269869+sachinseb@users.noreply.github.com>
2023-10-23 19:17:15 +0530

    Uploaded

---
+++ src/components/Contact.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <main>
+        <h1>Contact Us</h1>
+        <form>
+          <div>
+            <label>Name</label>
+            <input type="text" required placeholder="name" />
+          </div>
+
+          <div>
+            <label>Email</label>
+            <input type="email" required placeholder="name@gmail.com" />
+          </div>
+
+          <div>
+            <label>Message</label>
+            <input type="text" required placeholder="Give your message" />
+          </div>
+
+          <button type="submit">Send</button>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow us</h5>
+        <div>
+          <a href="https://www.youtube.com/@mysteriousdono" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/oncetechiene" target={"blank"}>
+            Instagram
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
